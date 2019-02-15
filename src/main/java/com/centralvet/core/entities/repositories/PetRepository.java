f42e5dcd020ae45ca89fd14aa48b504d957a42e6
Roberto Cantore <rcantore@gmail.com>
2019-02-15 08:48:18 -0300

    removed unnecessary methods

--- src/main/java/com/centralvet/core/entities/repositories/PetRepository.java
+++ src/main/java/com/centralvet/core/entities/repositories/PetRepository.java
@@ -1,6 +1,5 @@
 package com.centralvet.core.entities.repositories;
 
-import com.centralvet.core.entities.Customer;
 import com.centralvet.core.entities.Pet;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
@@ -8,16 +7,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PetRepository extends CrudRepository<Pet, Long> {
 
-    List<Pet> findAllByCustomer(Customer customer);
-
     Page<Pet> findAll(Example example, Pageable pageable);
-
-    Page<Pet> findAllByCustomer(Customer customer, Example example, Pageable pageable);
 
 }
 
